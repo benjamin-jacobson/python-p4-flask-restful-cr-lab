3e01648d3e56aa07c1b7d064de8a7f0fcec238a0
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-04-02 22:14:44 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -21,11 +21,26 @@ class Plants(Resource):
         response_dict_list = [p.to_dict() for p in Plant.query.all()]
         response = make_response(response_dict_list, 200)
         return response
+
+    def post(self):
+        data = request.get_json()
+        new_record = Plant(
+                name = data['name'],
+                image = data['image'],
+                price = data['price'])
+        db.session.add(new_record)
+        db.session.commit()
+        response_dict = new_record.to_dict()
+        response = make_response(response_dict,201)
+        return response
 api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self,id):
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(response_dict, 200)
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
